2078fb30543b30dab59a490c41e54dde66e4eda2
coilvoltic <volticthedev@gmail.com>
2026-01-19 21:37:24 +0100

    Ajust size on mobile

--- src/app/compte/page.tsx
+++ src/app/compte/page.tsx
@@ -559,14 +559,14 @@ export default function ComptePage() {
                   <h2 className="text-2xl font-bold text-black mb-2">
                     Connectez-vous avec Google
                   </h2>
-                  <p className="text-stone-600">
+                  <p className="text-base md:text-sm text-stone-600">
                     Accédez à toutes vos commandes en un clic
                   </p>
                 </div>
 
                 <button
                   onClick={handleGoogleSignIn}
-                  className="w-full bg-white border-2 border-stone-300 hover:border-black text-black font-medium py-4 px-6 rounded-xl transition-all flex items-center justify-center gap-3 group"
+                  className="w-full bg-white border-2 border-stone-300 hover:border-black text-black font-medium py-2 md:py-4 px-6 rounded-md md:rounded-xl transition-all flex items-center justify-center gap-3 group"
                 >
                   <svg className="w-6 h-6" viewBox="0 0 24 24">
                     <path
@@ -586,11 +586,11 @@ export default function ComptePage() {
                       d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                     />
                   </svg>
-                  <span className="text-lg">Se connecter avec Google</span>
+                  <span className="text-sm md:text-lg">Se connecter avec Google</span>
                 </button>
 
-                <div className="mt-6 text-center text-sm text-stone-500">
-                  Vos informations sont sécurisées et ne seront jamais partagées
+                <div className="mt-6 text-center text-base md:text-sm text-stone-500">
+                  Vos informations sont sécurisées et ne seront jamais partagées.
                 </div>
 
                 {/* Trait horizontal séparateur */}
