603dcd17bef579a046c2fc97c53ed530e264b5f4
coilvoltic <volticthedev@gmail.com>
2026-02-09 22:48:07 +0100

    Fix middleware matcher to allow public access to homepage

--- src/middleware.ts
+++ src/middleware.ts
@@ -306,5 +306,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+  matcher: ['/admin/:path*', '/((?!_next/static|_next/image|favicon.ico|admin).*)'],
 }
